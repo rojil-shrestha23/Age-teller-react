fb1d7c2740840ba605e696a021f905a1e6593203
rojil.shrestha <rojil.shrestha@wlink.com.np>
2018-07-11 22:38:02 +0545

    created app.css file to give custom css and here gives the style to move the date control to center

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Form, FormControl, Button} from 'react-bootstrap';
-
+import './App.css';
+ 
 class App extends React.Component{
     render() {
         return(
-            <div>
+            <div className= "App"> 
                 <Form inline>
                     <h2> Enter Your Birthday</h2>
                     <FormControl type = "date">
